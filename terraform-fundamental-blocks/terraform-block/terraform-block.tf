57dc745d28e7ca47696516bbf40be0318700fc4f
Sushant Chandra Kalia <sushantkalia0610@gmail.com>
2023-05-27 02:44:49 -0700

    terraform-fundamental-blocks

---
+++ terraform-fundamental-blocks/terraform-block/terraform-block.tf
@@ -0,0 +1,14 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.4"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.51"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+}
+}
