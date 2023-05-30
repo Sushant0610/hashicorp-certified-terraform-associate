f191f4454324a36098f23d4bb4ee24be91771c84
Sushant Chandra Kalia <sushantkalia0610@gmail.com>
2023-05-30 01:31:03 -0700

    terraform-resources

---
+++ terraform-resources/meta-argument-count/c2-ec2-instance.tf
@@ -0,0 +1,9 @@
+# Create EC2 Instance
+resource "aws_instance" "web" {
+  ami = "ami-0715c1897453cabd1"
+  instance_type = "t2.micro"
+  count = 5
+  tags = {
+    "Name" = "web-${count.index}"
+  }
+}
